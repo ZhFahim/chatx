0dc25e8ac29677cbe6bfded534bc7b17a3f792d1
ZhFahim <zhfahim.me@gmail.com>
2020-11-20 22:03:43 +0600

    Add option to see room members in room menu

--- lib/screens/roomMenu.dart
+++ lib/screens/roomMenu.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
 import 'package:chatx/widgets/leaveAlert.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chatx/constants.dart';
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class RoomMenuScreen extends StatelessWidget {
   RoomMenuScreen(this.roomId);
@@ -49,6 +53,69 @@ class RoomMenuScreen extends StatelessWidget {
                         );
                       },
                     ),
+                    StreamBuilder(
+                        stream: firestore
+                            .collection('rooms/$roomId/members')
+                            .orderBy('joinedAt')
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return ListTile(
+                              title: Text('Members'),
+                            );
+                          }
+                          final members = snapshot.data.documents;
+                          return ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            title: Text('Members (${snapshot.data.size})'),
+                            onTap: () {
+                              showModalBottomSheet(
+                                context: context,
+                                builder: (context) => Container(
+                                  child: Column(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () => Navigator.pop(context),
+                                        child: Container(
+                                          color: Theme.of(context).canvasColor,
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10.0),
+                                          width: double.maxFinite,
+                                          child:
+                                              Icon(Icons.keyboard_arrow_down),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: ListView.builder(
+                                          padding: EdgeInsets.all(20.0),
+                                          itemCount: members.length,
+                                          itemBuilder: (context, index) =>
+                                              ListTile(
+                                            contentPadding: EdgeInsets.zero,
+                                            title: Text(
+                                              members[index]['username'],
+                                              style: TextStyle(
+                                                  color: colors[members[index]
+                                                      ['username']]),
+                                            ),
+                                            subtitle: Text(
+                                              'Joined at: ${members[index]['joinedAt'].toDate().toLocal().toString().substring(0, 16)}',
+                                              style: TextStyle(
+                                                fontFamily:
+                                                    'HelveticaNeueLight',
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }),
                     ListTile(
                       contentPadding: EdgeInsets.zero,
                       title: Text(
